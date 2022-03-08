1f4534637c12480f639dafb5ca0ddccde4c090d7
lizzytag12 <luisa.h.morales@gmail.com>
2022-03-08 01:28:01 -0300

    homework is done

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const {dirname} = require ("path");
+const app = express();
+const port = 3030;
+const path = require("path");
+
+app.use(express.static(path.join (__dirname, "public")))
+
+
+app.get("/",(req,res)=>{
+
+    return res.sendFile(path.join(__dirname,"views","index.html"))
+
+})
+
+
+app.get("/babbage",(req,res)=>{
+
+    return res.sendFile(path.join(__dirname,"views","babbage.html"))
+
+})
+
+app.get("/berners-lee",(req,res)=>{
+
+    return res.sendFile(path.join(__dirname,"views","berners-lee.html"))
+
+})
+
+app.get("/clarke",(req,res)=>{
+
+    return res.sendFile(path.join(__dirname,"views","clarke.html"))
+
+})
+
+app.get("/hamilton",(req,res)=>{
+
+    return res.sendFile(path.join(__dirname,"views","hamilton.html"))
+
+})
+
+
+app.get("/hopper",(req,res)=>{
+
+    return res.sendFile(path.join(__dirname,"views","hopper.html"))
+
+})
+
+app.get("/lovelace",(req,res)=>{
+
+    return res.sendFile(path.join(__dirname,"views","lovelace.html"))
+
+})
+
+app.get("/turing",(req,res)=>{
+
+    return res.sendFile(path.join(__dirname,"views","turing.html"))
+
+})
+
+
+app.listen(port,()=>{
+    console.log( `your server is running on port http://localhost:${port}`);
+})
